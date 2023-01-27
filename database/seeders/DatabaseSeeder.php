13db866b18f999aea799afe6319a9c1ce416403f
jasonlerit <jasonlerit077@gmail.com>
2023-01-27 11:06:09 +0800

    updated database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Member;
+use App\Models\Membership;
+use App\Models\Trainer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,11 +18,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Member::factory(10)->create();
+        Trainer::create([
+            'name' => 'Trainer 1',
+            'email' => 'trainer1@gmail.com',
+            'specialization' => 'Lifestyle Wellness Coach',
+            'phone' => '123'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Trainer::create([
+            'name' => 'Trainer 2',
+            'email' => 'trainer2@gmail.com',
+            'specialization' => 'Senior Fitness Specialist',
+            'phone' => '123'
+        ]);
+
+        Trainer::create([
+            'name' => 'Trainer 3',
+            'email' => 'trainer3@gmail.com',
+            'specialization' => 'Youth Fitness Specialists',
+            'phone' => '123'
+        ]);
+
+        Membership::create([
+            'membership_type' => 'College',
+            'membership_price' => 200
+        ]);
+
+        Membership::create([
+            'membership_type' => 'Senior',
+            'membership_price' => 150
+        ]);
     }
 }
