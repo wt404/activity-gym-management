041a7a7b9c1137580224b95f2c8f88268274860c
jasonlerit <jasonlerit077@gmail.com>
2023-01-27 11:14:29 +0800

    updated trainer blade

--- resources/views/trainer.blade.php
+++ resources/views/trainer.blade.php
@@ -4,7 +4,7 @@
     <div class="pt-5">
         <div class="mb-3 d-flex justify-content-between align-items-center">
             <h1>
-                Trainer Details
+                {{ $member->name }}'s Trainer Details
             </h1>
             <div>
                 <a href={{ route('member.show', $member->id) }} class="btn btn-light">Go Back</a>
