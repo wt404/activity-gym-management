ee189a4dd26848da76bd1477a98b108a84ff52da
jasonlerit <jasonlerit077@gmail.com>
2023-01-27 09:23:48 +0800

    added member controller

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    public function index(Request $request)
+    {
+        // TODO: Display all members
+    }
+
+    public function create(Request $request)
+    {
+        // TODO: Return view -> create
+    }
+
+    public function store(Request $request)
+    {
+        // TODO: Store new member
+    }
+
+    public function show(Request $request)
+    {
+        // TODO: Return member by id
+    }
+
+    public function update(Request $request)
+    {
+        // TODO: Update member by id
+    }
+
+    public function destroy(Request $request)
+    {
+        // TODO: Delete member by id
+    }
+}
