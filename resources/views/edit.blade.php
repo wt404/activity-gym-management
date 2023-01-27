39e9652191cd57d502adfe8f9d7f3ca981a5c015
jasonlerit <jasonlerit077@gmail.com>
2023-01-27 11:36:51 +0800

    added select option

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -25,13 +25,27 @@
                             <label for="email" class="form-label">
                                 Email
                             </label>
-                            <input type="email" class="form-control" id="email" name="email" value="{{ $member->email }}">
+                            <input type="email" class="form-control" id="email" name="email" value="{{ $member->email }}" disabled>
                         </div>
                         <div class="mb-3">
                             <label for="membership_type" class="form-label">
                                 Membership Type
                             </label>
-                            <input type="text" class="form-control" id="membership_type" name="membership_type" value="{{ $member->membership_type }}">
+                            <select class="form-select" name="membership_id">
+                                @foreach ($memberships as $membership)
+                                    <option selected={{ $member->membership_id == $membership->id }} value="{{ $membership->id }}">{{ $membership->membership_type }} - {{ $membership->membership_price }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="trainer" class="form-label">
+                                Trainer
+                            </label>
+                            <select class="form-select" name="trainer_id">
+                                @foreach ($trainers as $trainer)
+                                    <option selected={{ $member->trainer_id == $trainer->id }} value="{{ $trainer->id }}">{{ $trainer->name }} - {{ $trainer->specialization }}</option>
+                                @endforeach
+                            </select>
                         </div>
                         <button type="submit" class="btn btn-primary">Update</button>
                     </form>
