dca3cf489538c57ffa03976a57d9bc98a6a98aa6
jasonlerit <jasonlerit077@gmail.com>
2023-01-27 10:53:54 +0800

    added trainer blade

---
+++ resources/views/trainer.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts/layout')
+
+@section('content')
+    <div class="pt-5">
+        <div class="mb-3 d-flex justify-content-between align-items-center">
+            <h1>
+                Trainer Details
+            </h1>
+            <div>
+                <a href={{ route('member.show', $member->id) }} class="btn btn-light">Go Back</a>
+            </div>
+        </div>
+        <div>
+            <div class="card text-black">
+                <div class="card-body">
+                    <p>
+                        Name: {{ $member->trainer->name }}
+                    </p>
+                    <p>
+                        Email: {{ $member->trainer->email }}
+                    </p>
+                    <p>
+                        Specialization: {{ $member->trainer->specialization }}
+                    </p>
+                    <p>
+                        Phone: {{ $member->trainer->phone }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
