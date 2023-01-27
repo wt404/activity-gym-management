1c75f430bfef131dcf2f7e1120bcbebafe0d18aa
jasonlerit <jasonlerit077@gmail.com>
2023-01-27 11:01:00 +0800

    fixed migration typo

--- database/migrations/2023_01_27_011631_create_memberships_table.php
+++ database/migrations/2023_01_27_011631_create_memberships_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('membership', function (Blueprint $table) {
+        Schema::create('memberships', function (Blueprint $table) {
             $table->id();
             $table->string('membership_type');
             $table->double('membership_price');
