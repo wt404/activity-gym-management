3319a7b3f9844df6fbb77dba055ad0833ab421de
jasonlerit <jasonlerit077@gmail.com>
2023-01-27 09:26:12 +0800

    added layout

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        Gym Management
+    </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+<body class="bg-dark text-white">
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
