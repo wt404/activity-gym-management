7e70abd35973a5237ef0c16d9d24e134ac2bea4c
jasonlerit <jasonlerit077@gmail.com>
2023-01-27 10:15:39 +0800

    updated show blade

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
     <div class="pt-5">
-        Show
+        <div class="mb-3 d-flex justify-content-between align-items-center">
+            <h1>
+                Member Details
+            </h1>
+            <div>
+                <a href={{ route('member.index') }} class="btn btn-light">Go Back</a>
+            </div>
+        </div>
+        <div>
+            <div class="card text-black">
+                <div class="card-body">
+                    <p>
+                        Name: {{ $member->name }}
+                    </p>
+                    <p>
+                        Email: {{ $member->email }}
+                    </p>
+                    <p>
+                        Membership Type: {{ $member->membership_type }}
+                    </p>
+                    <p>
+                        Membership Expiration: {{ $member->membership_expiration }}
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 @stop
